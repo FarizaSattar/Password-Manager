9c74251668f296e5a863fcbf2f1ffa2dcbdecea1
Fariza Sattar <76606128+FarizaSattar@users.noreply.github.com>
2024-08-21 09:18:40 -0400

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,8 @@
+print("Welcome to the password manager!")
+
+input("Please enter your username.")
+input("Please enter your password.")
+
+print("Here are a list of your current services, usernames, and passwords.")
+
+input("Would you like to add, edit, or delete any entries?")
