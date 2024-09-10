4c417374b2ed9c8eb2190f37ab46df50f20366e5
Fariza Sattar <76606128+FarizaSattar@users.noreply.github.com>
2024-09-10 12:20:47 -0400

    Delete main2.py

--- main2.py
+++
@@ -1,8 +0,0 @@
-print("Welcome to the password manager!")
-
-input("Please enter your username.")
-input("Please enter your password.")
-
-print("Here are a list of your current services, usernames, and passwords.")
-
-input("Would you like to add, edit, or delete any entries?")
